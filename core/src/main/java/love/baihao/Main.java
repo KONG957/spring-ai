1631b1b37e1c35abe60e67a903288a8cd0b40432
baihao <hao.bai@tongdun.net>
2024-08-01 16:15:31 +0800

    feat:springboot and springAi dependency import

---
+++ core/src/main/java/love/baihao/Main.java
@@ -0,0 +1,11 @@
+package love.baihao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
